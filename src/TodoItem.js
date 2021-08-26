b9584b5d7daede482f37a41018b87a9be4bde304
Christianlopez4 <1107525635@u.icesi.edu.co>
2021-08-26 14:47:38 -0500

    Eventos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,19 +2,32 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-    return (
-      <li className="TodoItem">
-        <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
-          √
-        </span>
-        <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
-          {props.text}
-        </p>
-        <span className="Icon Icon-delete">
-          X
-        </span>
-      </li>
-    );
-  }
-  
-  export { TodoItem };
+
+  const onComplete = () => {
+    alert('Completaste el Todo: ' + props.text)
+  };
+
+  const onDelete = () => {
+    alert('Eliminaste el Todo: ' + props.text)
+  };
+
+  return (
+    <li className="TodoItem">
+      <span
+        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
+        onClick={onComplete}>
+        √
+      </span>
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+        {props.text}
+      </p>
+      <span
+        className="Icon Icon-delete"
+        onClick={onDelete}>
+        X
+      </span>
+    </li>
+  );
+}
+
+export { TodoItem };
