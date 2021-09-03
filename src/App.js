06b1d5e9d1e6d2b8127ad1b0aa061ddcff31e4f2
Christianlopez4 <1107525635@u.icesi.edu.co>
2021-09-03 09:42:30 -0500

    Contando y buscando todos

--- src/App.js
+++ src/App.js
@@ -6,20 +6,44 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar curso intro react', completed: false },
+  { text: 'Tomar curso intro react', completed: true },
   { text: 'Practicar inglés', completed: false }
 ];
 
 function App() {
+
+  const [searchValue, setSearchValue] = React.useState('');
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        completed={completedTodos}
+        total={totalTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map( todo => (
+        {searchedTodos.map( todo => (
           <TodoItem 
             key={todo.text} 
             text={todo.text}
